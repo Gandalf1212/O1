47c408805d9f563896eca682e6489f616f989c2c
Gandalf1212 <doollarpodnimaetca@mail.ru>
2020-10-05 12:19:49 +0300

    a

--- la1/search.sh
+++ la1/search.sh
@@ -4,74 +4,34 @@ function ERROR_arguments(){
     exit -1
 }
 
-function FILE_ERROR(){
-    echo "File: $1.sh  -  FILE ERROR"
+function ERROR_rights(){
+    echo "$1"
+    echo  "This directory is not available for you"
     exit -2
 }
 
-function Access_ERROR(){
-    echo "File: $1.sh  -  ACCESS ERROR"
+function ERROR_exist(){
+    echo "$1"
+    echo "Directory not exists"
     exit -3
 }
 
-function empty_main(){
-    if [[ $1 = '' ]]
-    then 
-    echo "YOU ENTER EMTY ARGUMENT, TAB 'HELP' TO USE CLOSSARY"
-    exit -4
+function check_correct(){
+    if [[ -n $4 || -z $3 || -z $2 ]]
+    then ERROR_arguments
     fi
-}
-
-function checkFile(){
-    if ! [ -a $1.sh ] 
-    then
-    File_ERROR $1
+    if ! [[ -e $2 ]]
+    then ERROR_exist $2
     fi
-    if ! [ -r $1.sh ]
-    then 
-    Access_ERROR
+    if ! [[ -r $2 ]]
+    then ERROR_rights $2
     fi
 }
 
 function DO(){
-    case $1 in
-    calc)      
-        ./calc.sh $@
-        ;;
-    search)
-        ./search.sh $@
-        ;;
-    reverse)
-        ./reverse.sh $@
-        ;;
-    log)
-        ./log.sh $@
-        ;;
-    exit)
-        ./exit.sh $@
-        ;;
-    help)
-        ./help.sh $@
-        ;;
-    interactive)
-        ./interactive.sh $@
-        ;;
-    esac
+    grep -r -s "$3" "$2"
 }
 
-empty_main $1
-checkFile $1
-if [ $1 = "strlen" ]
-then 
-    if ! [[ $3 = "int" ]]
-    then
-        if ! [[ $# -eq 2 ]]
-        then 
-        ERROR_arguments
-        fi
-        ./strlen.sh "$2"
-        exit 0
-    fi
-./strlen.sh "$2" $3
-fi
+echo "!-search-!"
+check_correct $@
 DO $@
