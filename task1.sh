9c3eac8a9b53e7d16b9cf49813a8d681ad696847
Gandalf1212 <doollarpodnimaetca@mail.ru>
2020-12-11 12:36:52 +0300

    a

--- task1.sh
+++ task1.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 dataT=$(date "+%d.%m.%y_%T")
 mkdir ~/test && echo "catalog test was created successfully" > ~/report && touch ~/test/$dataT.txt
-ping www.net_nikogo.ru | echo "$dataT ERROR" >> ~/report
+ping www.net_nikogo.ru || echo "$dataT ERROR" >> ~/report
