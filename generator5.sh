7ed75c77508d956d49090f913b19e0f92776b07a
Gandalf1212 <71775953+Gandalf1212@users.noreply.github.com>
2020-12-31 01:52:29 +0300

    Update generator5.sh

--- generator5.sh
+++ generator5.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 while true
-do
-    read line
-    echo $line > pipe5
+	do
+	read -r line
+	echo "$line" >> pipe
+	[[ "$line" == "QUIT" ]] && then exit 0
+	[[ "$line" != "+" && "$line" != "*" && ! "$line" =~ [0-9]+ ]] && {
+		echo "error input"
+		exit 1;
+	}
 done
-
