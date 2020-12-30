ea19b85cace02c842c73c159dcb1c64830a46101
Gandalf1212 <71775953+Gandalf1212@users.noreply.github.com>
2020-12-31 00:38:22 +0300

    Update task4.sh

--- task4.sh
+++ task4.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
 ./taska4.sh & FirstPID=$!
+cpulimit -p $FirstPID -l 5 -b -z
 ./taska4.sh & SecondPID=$!
 ./taska4.sh & ThirdPID=$!
 echo "1st PID" $FirstPID
 echo "2nd PID" $SecondPID
 echo "3d PID" $ThirdPID
-renice +10 -p $FirstPID
 kill $ThirdPID
