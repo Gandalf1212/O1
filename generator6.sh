bfe7d31a6a9b3cbcc13888656187ef4d98ecde1b
Gandalf1212 <71775953+Gandalf1212@users.noreply.github.com>
2020-12-31 02:01:48 +0300

    Update generator6.sh

--- generator6.sh
+++ generator6.sh
@@ -3,15 +3,15 @@ while true
 do
       read line
       case $line in
-      "TERM")
-             kill -SIGTERM $(cat pipe6)
-             exit
-      ;;
       "+")
             kill -USR1 $(cat pipe6)
       ;;
       "*")
            kill -USR2 $(cat pipe6)
+      ;;
+            "TERM")
+             kill -SIGTERM $(cat pipe6)
+             exit
       ;;
       esac
 done
